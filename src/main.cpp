e1839621f327efbb587fea3bfee2e76ee006375b
NJL7502L <g.hana1211.tako@gmail.com>
2021-07-25 14:34:51 +0900

    上手くwebserialが動作にした

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+#include "secret.h"
+
+AsyncWebServer server(80);
+
+const char *ssid = WIFI_SSID;         // Your WiFi SSID
+const char *password = WIFI_PASSWORD; // Your WiFi Password
+
+void recvMsg(uint8_t *data, size_t len) {
+  WebSerial.println("Received Data...");
+  String d = "";
+  for (int i = 0; i < len; i++) {
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+}
+
+void setup() {
+  delay(3000);
+  Serial.begin(115200);
+  Serial.println("Hello world!!");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg);
+  server.begin();
+}
+
+void loop() {}
