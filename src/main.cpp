68df08dd7f982be0a59c46b955b92cd6daa873f8
NJL7502L <g.hana1211.tako@gmail.com>
2021-07-25 15:32:26 +0900

    hx711のキャリブレーション値をブラウザからリモートで見れるようになった

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,33 @@
 #include <ESPAsyncWebServer.h>
 #include <WebSerial.h>
 #include "secret.h"
+#include "HX711.h"
 
 AsyncWebServer server(80);
 
 const char *ssid = WIFI_SSID;         // Your WiFi SSID
 const char *password = WIFI_PASSWORD; // Your WiFi Password
 
+#define DOUT 0
+#define CLK 2
+HX711 scale;
+
+int cursor = 0;
+int ch_f = 0;
+float caliblation_factor = 0;
+
 void recvMsg(uint8_t *data, size_t len) {
   WebSerial.println("Received Data...");
   String d = "";
   for (int i = 0; i < len; i++) {
     d += char(data[i]);
+  }
+  if (data[0] == 'n') {
+    ch_f = 1;
+    cursor++;
+  } else if (data[0] == 'p') {
+    ch_f = 1;
+    cursor--;
   }
   WebSerial.println(d);
 }
@@ -35,6 +51,48 @@ void setup() {
   WebSerial.begin(&server);
   WebSerial.msgCallback(recvMsg);
   server.begin();
+
+  scale.begin(DOUT, CLK);
 }
 
-void loop() {}
+void loop() {
+  if (ch_f == 1) {
+    switch (cursor) {
+    case 1:
+      WebSerial.println("hello webSerial");
+      ch_f = 0;
+      break;
+
+    case 2:
+      WebSerial.println("hx711 reset");
+      scale.set_scale();
+      scale.tare();
+      WebSerial.println("done. you can set load");
+      ch_f = 0;
+      break;
+
+    case 3:
+      WebSerial.print(scale.get_units(10));
+      WebSerial.print("kg @ ");
+      WebSerial.println(caliblation_factor);
+      break;
+
+    case 4:
+      caliblation_factor = scale.get_units(10) / 12.0f;
+      scale.set_scale(caliblation_factor);
+      WebSerial.print("done. caliblation_factor is ");
+      WebSerial.println(caliblation_factor);
+      ch_f = 0;
+      break;
+
+    case 5:
+      WebSerial.println("reset cursor");
+      cursor = 0;
+      ch_f = 0;
+      break;
+
+    default:
+      break;
+    }
+  }
+}
